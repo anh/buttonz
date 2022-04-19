fe0e0a8a5e293e8788f83fc2a39e5a61aabd8260
anh <anhlh2@gmail.com>
2022-04-20 02:24:53 +0700

    Fix test case

--- tests/comments_test.js
+++ tests/comments_test.js
@@ -7,8 +7,9 @@ import { parse_buttons } from '../src/cards.js';
 test('skip comments and blank line', t => {
     let code = parse_buttons('tests/comments.btn');
     t.deepEqual(code,
-        [ ['PUSH', '1'],
-          ['POP']
+        [ ['PUSH', 1],
+          ['POP'],
+          ['STOR', 'AX']
         ]
     )
 })
